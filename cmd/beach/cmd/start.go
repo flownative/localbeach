c687c88b5a200d5e59e2f045623e55d625e9bbbd
Karsten Dambekalns <karsten@dambekalns.de>
2020-09-28 21:56:35 +0200

    When starting an instance, start db & nginx if neededd

--- cmd/beach/cmd/start.go
+++ cmd/beach/cmd/start.go
@@ -1,4 +1,5 @@
 // Copyright © 2019 - 2020 Robert Lemke / Flownative GmbH
+// Copyright © 2020 Karsten Dambekalns / Flownative GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,10 +17,13 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/flownative/localbeach/pkg/beachsandbox"
 	"github.com/flownative/localbeach/pkg/exec"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"strings"
+	"time"
 )
 
 var pull bool
@@ -29,8 +33,8 @@ var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start the Local Beach instance in the current directory",
 	Long:  "",
-	Args: cobra.ExactArgs(0),
-	Run: handleStartRun,
+	Args:  cobra.ExactArgs(0),
+	Run:   handleStartRun,
 }
 
 func init() {
@@ -42,6 +46,12 @@ func handleStartRun(cmd *cobra.Command, args []string) {
 	commandArgs := []string{""}
 
 	sandbox, err := beachsandbox.GetActiveSandbox()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = startLocalBeach()
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -66,7 +76,7 @@ func handleStartRun(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	commandArgs = []string{"exec", "local_beach_database" ,"/bin/bash" ,"-c", "echo 'CREATE DATABASE IF NOT EXISTS `" + sandbox.ProjectName + "`' | mysql -u root --password=password"}
+	commandArgs = []string{"exec", "local_beach_database", "/bin/bash", "-c", "echo 'CREATE DATABASE IF NOT EXISTS `" + sandbox.ProjectName + "`' | mysql -u root --password=password"}
 	_, err = exec.RunCommand("docker", commandArgs)
 	if err != nil {
 		log.Fatal(err)
@@ -76,4 +86,37 @@ func handleStartRun(cmd *cobra.Command, args []string) {
 	log.Info("You are all set")
 	log.Info("When files have been synced, you can access this instance at http://" + sandbox.ProjectName + ".localbeach.net")
 	return
+}
+
+func startLocalBeach() error {
+	output, err := exec.RunCommand("docker-compose", []string{"-f", "/usr/local/lib/beach-cli/localbeach/docker-compose.yml", "ps", "-q"})
+	if err != nil {
+		return err
+	}
+	if len(output) == 0 {
+		log.Info("Starting nginx & database ...")
+		commandArgs := []string{"-f", "/usr/local/lib/beach-cli/localbeach/docker-compose.yml", "up", "--remove-orphans", "-d"}
+		err = exec.RunInteractiveCommand("docker-compose", commandArgs)
+		if err != nil {
+			return err
+		}
+
+		log.Info("Waiting for database ...")
+		tries := 1
+		for {
+			output, err := exec.RunCommand("docker", []string{"inspect", "-f", "{{.State.Health.Status}}", "local_beach_database"})
+			if err != nil {
+				return err
+			}
+			if strings.TrimSpace(output) == "healthy" {
+				break
+			}
+			if tries == 10 {
+				return errors.New("timeout waiting for database to start")
+			}
+			tries++
+			time.Sleep(3 * time.Second)
+		}
+	}
+	return nil
 }
